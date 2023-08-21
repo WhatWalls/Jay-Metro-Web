88838cb6408b52c1fd350023f4ed930f5cdef35a
whatwalls <415davmill@gmail.com>
2023-08-21 02:16:24 -0400

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'sm': { 'max': '700px' },
+        'sm': { 'max': '1600' },
         'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
         'tallscreen': { 'raw': '(max-aspect-ratio: 13/20)' }
       },
